b0ac8be258fae33335ef027d2f43b2eb34531d91
James Gamble <jamesleongamble@gmail.com>
2025-01-28 20:31:05 -0700

    =update

---
+++ elpa/volume-20220904.1727/volume-pkg.el
@@ -0,0 +1,9 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "volume" "20220904.1727"
+  "Tweak your sound card volume from Emacs."
+  ()
+  :url "http://www.brockman.se/software/volume-el/"
+  :commit "050d3e6d2543a6771a13f95612055864679b6301"
+  :revdesc "050d3e6d2543"
+  :authors '(("Daniel Brockman" . "daniel@brockman.se"))
+  :maintainers '(("Daniel Brockman" . "daniel@brockman.se")))
