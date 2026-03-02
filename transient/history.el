4fd239ffbfe9c0943199ce1ae0ee2677f261c66b
firebox <firebox@DESKTOP-S8TKTKE.search.charter.net>
2026-03-02 14:34:10 -0700

    Add .gitignore

--- transient/history.el
+++ transient/history.el
@@ -1,4 +1,14 @@
 ((magit-am
   ("--3way"))
+ (magit-commit
+  ("--all"))
+ (magit-diff
+  ("--no-ext-diff" "--stat"))
+ (magit-fetch nil)
+ (magit-merge nil)
  (magit-patch-apply nil)
+ (magit-pull
+  ("--force")
+  ("--rebase"))
+ (magit-push nil)
  (magit-status-jump nil))
